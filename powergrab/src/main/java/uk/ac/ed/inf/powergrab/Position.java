5af088a9dcf06f90619d58742b2bce16dbf5df7b
Liana <lianafaahmed@gmail.com>
2019-09-26 11:57:16 +0100

    First part is now done

--- powergrab/src/main/java/uk/ac/ed/inf/powergrab/Position.java
+++ powergrab/src/main/java/uk/ac/ed/inf/powergrab/Position.java
@@ -17,25 +17,15 @@ public class Position {
 		double new_latitude = 0;
 		double new_longitude = 0;
 		
-		// If the direction is between North and East
-		if(da >= 0 && da <= 112.5) {
+		new_longitude = longitude + r*(Math.cos(Math.toRadians(90-da)));
+		new_latitude = latitude + r*(Math.sin(Math.toRadians(90-da)));
 			
-			new_longitude = longitude + r*(Math.cos(Math.toRadians(90-da)));
-			new_latitude = latitude + r*(Math.sin(Math.toRadians(90-da)));
-			
-		// If the direction is between South and West
-		} else if(da >= 135 && da <= 337.5) {
-			
-			new_longitude = longitude - r*(Math.cos(Math.toRadians(90-da)));
-			new_latitude = latitude - r*(Math.sin(Math.toRadians(90-da)));
-			
-		}
 		
 		return new Position(new_latitude, new_longitude);
 	}
 	
 	public boolean inPlayArea() {
-		if((latitude >= 55.942617 && latitude <= 55.946233) && (longitude >= -3.192473 && longitude <= -3.184319)) {
+		if((latitude > 55.942617 && latitude < 55.946233) && (longitude > -3.192473 && longitude < -3.184319)) {
 			return true;
 		} else {
 			return false;
